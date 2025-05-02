2707c87f73efad40e365fdbe84da571824f0e024
Archilipeper <p.archila@uniandes.edu.co>
2025-05-02 07:45:42 -0500

    Primeras funciones, aún no funcionan

--- DataStructures/Priority_queue/priority_queue.py
+++ DataStructures/Priority_queue/priority_queue.py
@@ -1,3 +1,25 @@
-from DataStructures.List import array_list as lt
+from DataStructures.List import array_list as al
 
+def new_heap(is_min_pq=True):
+    if is_min_pq == True:
+        cmp_function = default_compare_lower_value()
+    else:
+        cmp_function = default_compare_higher_value()
+    lista = al.new_list()
+    al.add_last(lista, None)
+    my_heap = {"elelemts": lista,
+               "size": 0,
+               "cmp_function": cmp_function}
+    return my_heap
 
+def default_compare_higher_value(father_node, child_node):
+    if father_node >= child_node:
+        return True
+    else:
+        return False
+
+def default_compare_lower_value(father_node, child_node):
+    if father_node <= child_node:
+        return True
+    else:
+        return False
